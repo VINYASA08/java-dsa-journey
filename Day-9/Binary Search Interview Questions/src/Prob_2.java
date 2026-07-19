df3866e068b0a0bffaf9c3b5994f926befbb8f42
Vinod kumar <vinyasa333@gmail.com>
2026-07-19 20:51:17 +0530

    feat: Do some good problems on Binary search algorithm in 1D array

---
+++ Day-9/Binary Search Interview Questions/src/Prob_2.java
@@ -0,0 +1,27 @@
+public class Prob_2 {
+    static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8,9,10,12,13,15,16,18};
+        int target=14;
+        System.out.print(floor(arr,target));
+    }
+
+    static int floor(int[] arr , int target){
+        int close=0;
+        int low=0;
+        int high=arr.length-1;
+
+        while (low<=high){
+
+            int mid=low+(high-low)/2;
+
+            if(target<arr[mid]){
+                high=mid-1;
+            }else if (target>arr[mid]){
+                low=mid+1;
+            }else {
+                return arr[mid];
+            }
+        }
+        return arr[high];
+    }
+}
---
+++ Day-9/Binary Search Interview Questions/src/Prob_3.java
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+public class Prob_3 {
+    static void main(String[] args) {
+        int[] nums={5,7,7,8,8,10};
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+
+        int[] ans = {-1, -1};
+        ans[0] = search(nums, target, true);
+        if (ans[0] != -1) {
+            ans[1] = search(nums, target, false);
+        }
+        return ans;
+    }
+
+    int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
